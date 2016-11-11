a7ec7c4811c2599175f62bfc27ec6623cc598d19
Gertjan Al <gertjan@holmes.nl>
2016-11-11 16:04:30 +0100

    Added signatures

--- metal-tools-formats/src/main/java/nl/gertjanal/metaltools/formats/rar/RAR.java
+++ metal-tools-formats/src/main/java/nl/gertjanal/metaltools/formats/rar/RAR.java
@@ -1,13 +1,16 @@
 package nl.gertjanal.metaltools.formats.rar;
 
+import static io.parsingdata.metal.Shorthand.cho;
 import static io.parsingdata.metal.Shorthand.con;
-import static io.parsingdata.metal.Shorthand.nod;
+import static io.parsingdata.metal.Shorthand.def;
+import static io.parsingdata.metal.Shorthand.eq;
 
 import io.parsingdata.metal.token.Token;
 
 /**
  * The RAR archive format.
- * {@link
+ * RAR 5.0: {@link http://www.rarlab.com/technote.htm}
+ * RAR 4.x: {@link http://www.forensicswiki.org/wiki/RAR}
  *
  * @author Netherlands Forensic Institute.
  */
@@ -17,5 +20,7 @@ public class RAR {
 	public static final int UINT32 = 32 / 8;
 	public static final int UINT64 = 64 / 8;
 
-	public static final Token FORMAT = nod(con(0));
+	public static final Token FORMAT = cho(
+		def("rar 5 signature", 8, eq(con(0x52, 0x61, 0x72, 0x21, 0x1A, 0x07, 0x01, 0x00))),
+		def("rar 4 signature", 7, eq(con(0x52, 0x61, 0x72, 0x21, 0x1A, 0x07, 0x00))));
 }
