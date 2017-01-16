9fbc3247c3513f0dfcc8dda351b83dbe411452b1
Gertjan Al <gertjan@holmes.nl>
2017-01-16 11:26:00 +0100

    Removed old test

--- metal-tools-formats/src/test/java/nl/gertjanal/metaltools/formats/rar/RARTest.java
+++ metal-tools-formats/src/test/java/nl/gertjanal/metaltools/formats/rar/RARTest.java
@@ -18,11 +18,4 @@ public class RARTest {
 		final ParseResult result = RAR.FORMAT.parse(environment, le());
 		assertTrue(result.succeeded);
 	}
-
-	@Test
-	public void testRAR5() throws Exception {
-		final Environment environment = ResourceEnvironment.environment("/rar/example5.40.rar");
-		final ParseResult result = RAR.FORMAT.parse(environment, le());
-		assertTrue(result.succeeded);
-	}
 }
